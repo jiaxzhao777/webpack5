74d49d1e41d9887bec003d399d4bd205016251ff
jiaxzhao777 <jiaxzhao@ebay.com>
2024-03-24 15:10:05 +0800

    feat: dev server

--- webpack.config.js
+++ webpack.config.js
@@ -20,35 +20,44 @@ module.exports = {
           options: { presets: ["@babel/preset-typescript"] },
         },
       },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
-          },
-          "postcss-loader",
-        ],
-      },
       // {
       //   test: /\.css$/,
       //   use: [
-      //     // 根据运行环境判断使用那个 loader
-      //     process.env.NODE_ENV === "development"
-      //       ? "style-loader"
-      //       : MiniCssExtractPlugin.loader,
-      //     "css-loader",
+      //     MiniCssExtractPlugin.loader,
+      //     {
+      //       loader: "css-loader",
+      //       options: {
+      //         importLoaders: 1,
+      //       },
+      //     },
+      //     "postcss-loader",
       //   ],
       // },
+      {
+        test: /\.css$/,
+        use: [
+          // 根据运行环境判断使用那个 loader
+          process.env.NODE_ENV === "development"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
+          "css-loader",
+        ],
+      },
     ],
   },
   plugins: [
     new ESLintPlugin({ extensions: [".js", ".ts"] }),
     new MiniCssExtractPlugin(),
-    new HTMLWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
     require("autoprefixer"),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3000,
+  },
 };
